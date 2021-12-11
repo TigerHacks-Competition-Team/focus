8e9701fc982c19bfd4da1c35e44b0aefe94b7386
VirtualMe64 <43835028+VirtualMe64@users.noreply.github.com>
2021-12-11 18:54:29 -0500

    added face outline tracking

--- src/Demo.js
+++ src/Demo.js
@@ -2,61 +2,92 @@ import "@tensorflow/tfjs-core";
 import "@tensorflow/tfjs-converter";
 import "@tensorflow/tfjs-backend-webgl";
 import * as faceLandmarksDetection from "@tensorflow-models/face-landmarks-detection";
-import React, { useEffect, useRef, useState } from "react"
+import React, { useEffect, useRef, useState } from "react";
 import Webcam from "react-webcam";
 import { draw } from "./mask";
 
 const Demo = () => {
-    const videoRef = useRef(null)
-    const canvasRef = useRef(null)
-    const [frames, setFrames] = useState(0)
-    
-    const runPredict = async () => {
-        console.log("loading model")
-        const model = await faceLandmarksDetection.load(
-            faceLandmarksDetection.SupportedPackages.mediapipeFacemesh
-        )
-        console.log("detecting")
-        detect(model)
-    }
-    const detect = async (model) => {
-        const t0 = new Date().getTime()
-        if (videoRef.current) {
-          const webcamCurrent = videoRef.current;
-          const videoWidth = webcamCurrent.video.videoWidth;
-        const videoHeight = webcamCurrent.video.videoHeight;
-        canvasRef.current.width = videoWidth;
-        canvasRef.current.height = videoHeight;
-          // go next step only when the video is completely uploaded.
-          if (webcamCurrent.video.readyState === 4) {
-            const video = webcamCurrent.video;
-            const predictions = await model.estimateFaces({
-              input: video,
-            });
-            if (predictions.length) {
-              console.log(predictions);
-              const ctx = canvasRef.current.getContext("2d")
-              requestAnimationFrame(() => {
-                  console.log("drawing")
-                  draw(predictions, ctx, videoWidth, videoHeight)
-              })
-              
-              const t1 = new Date().getTime()
-              setFrames(1/((t1-t0)/1000))
-              //detect(model)
-            }
-          }
-        };
-      };
-    useEffect(() => {
-        runPredict()
-    }, [videoRef])
-    
- return <div>
- <Webcam id="video" ref={videoRef}></Webcam>
- <canvas ref={canvasRef} />
- <p>{frames}</p>
-</div>  
-}
+  const videoRef = useRef(null);
+  const canvasRef = useRef(null);
+  const [frames, setFrames] = useState(0);
+  const [tog, setTog] = useState(false);
+  const [first, setFirst] = useState(true);
+  const [done, setDone] = useState(false);
+  const [loaded, setLoaded] = useState(false);
+  const [model, setModel] = useState(null);
 
-export default Demo
+  const loop = true;
+  const consoleOuput = true;
+
+  const runPredict = async () => {
+    if (consoleOuput) {
+      console.log("loading model");
+    }
+    setModel(
+      await faceLandmarksDetection.load(
+        faceLandmarksDetection.SupportedPackages.mediapipeFacemesh
+      )
+    );
+    setLoaded(true);
+    detect(model);
+  };
+  const detect = async (model) => {
+    const t0 = new Date().getTime();
+    if (videoRef.current) {
+      const webcamCurrent = videoRef.current;
+      const videoWidth = webcamCurrent.video.videoWidth;
+      const videoHeight = webcamCurrent.video.videoHeight;
+      // go next step only when the video is completely uploaded.
+      if (webcamCurrent.video.readyState === 4) {
+        if (first) {
+          setFirst(false);
+          canvasRef.current.width = videoWidth;
+          canvasRef.current.height = videoHeight;
+        }
+        const video = webcamCurrent.video;
+        const predictions = await model.estimateFaces({
+          input: video,
+        });
+        if (predictions.length) {
+          if (consoleOuput) {
+            console.log(predictions);
+          }
+          if (!loop) {
+            setDone(true);
+          }
+          const ctx = canvasRef.current.getContext("2d");
+          requestAnimationFrame(() => {
+            if (consoleOuput) {
+              console.log("drawing");
+            }
+            draw(predictions, ctx, videoWidth, videoHeight);
+          });
+
+          const t1 = new Date().getTime();
+          setFrames(1 / ((t1 - t0) / 1000));
+          //detect(model)
+        }
+      }
+    }
+    setTog(!tog);
+  };
+  useEffect(() => {
+    if (!done) {
+      if (model === null) {
+        runPredict();
+      } else {
+        detect(model);
+      }
+    }
+  }, [videoRef, tog]);
+
+  return (
+    <div>
+      <Webcam id="video" ref={videoRef}></Webcam>
+      <canvas ref={canvasRef} />
+      <p>{frames}</p>
+    </div>
+  );
+};
+
+export default Demo;
--- src/mask.js
+++ src/mask.js
@@ -6,74 +6,90 @@ import {
   Coords3D,
 } from "@tensorflow-models/face-landmarks-detection/dist/mediapipe-facemesh/util";
 
-const drawMask = (
-  ctx,
-  keypoints,
-  distance
-) => {
-  const points = [
-    93,
-    132,
-    58,
-    172,
-    136,
-    150,
-    149,
-    176,
-    148,
-    152,
-    377,
-    400,
-    378,
-    379,
-    365,
-    397,
-    288,
-    361,
-    323,
-  ];
-
-  ctx.moveTo(keypoints[195][0], keypoints[195][1]);
-  for (let i = 0; i < points.length; i++) {
-    if (i < points.length / 2) {
-      ctx.lineTo(
-        keypoints[points[i]][0] - distance,
-        keypoints[points[i]][1] + distance
-      );
-    } else {
-      ctx.lineTo(
-        keypoints[points[i]][0] + distance,
-        keypoints[points[i]][1] + distance
-      );
-    }
-  }
+const facePoints = {
+  leftEye: [
+    263, 249, 390, 373, 374, 380, 381, 382, 263, 466, 388, 387, 386, 385, 384,
+    398, 263,
+  ],
+  leftIris: [474, 475, 476, 477, 474],
+  leftEyeBrow: [276, 283, 282, 295, 300, 293, 334, 296, 276],
+  rightEye: [
+    33, 7, 163, 144, 145, 153, 154, 155, 33, 246, 161, 160, 159, 158, 157, 173,
+    33,
+  ],
+  rightIris: [469, 470, 471, 472, 469],
+  rightEyeBrow: [46, 53, 52, 65, 70, 63, 105, 66, 46],
+  lips: [
+    61, 146, 91, 181, 84, 17, 314, 405, 321, 375, 61, 185, 40, 39, 37, 0, 267,
+    269, 270, 409, 78, 95, 88, 178, 87, 14, 317, 402, 318, 324, 78, 191, 80, 81,
+    82, 13, 312, 311, 310, 415, 61,
+  ],
+  faceOval: [
+    10, 338, 297, 332, 284, 251, 389, 356, 454, 323, 361, 288, 397, 365, 379,
+    378, 400, 377, 152, 148, 176, 149, 150, 136, 172, 58, 132, 93, 234, 127,
+    162, 21, 54, 103, 67, 109, 10,
+  ],
 };
 
-export const draw = (
-  predictions,
-  ctx,
-  width,
-  height
-) => {
+const annotationKeys = [
+  "silhouette",
+  "lipsUpperOuter",
+  "lipsLowerOuter",
+  "lipsUpperInner",
+  "lipsLowerInner",
+  "rightEyeUpper0",
+  "rightEyeLower0",
+  "rightEyeUpper1",
+  "rightEyeLower1",
+  "rightEyeUpper2",
+  "rightEyeLower2",
+  "rightEyeLower3",
+  "rightEyebrowUpper",
+  "rightEyebrowLower",
+  "rightEyeIris",
+  "leftEyeUpper0",
+  "leftEyeLower0",
+  "leftEyeUpper1",
+  "leftEyeLower1",
+  "leftEyeUpper2",
+  "leftEyeLower2",
+  "leftEyeLower3",
+  "leftEyebrowUpper",
+  "leftEyebrowLower",
+  "leftEyeIris",
+  "midwayBetweenEyes",
+  "noseTip",
+  "noseBottom",
+  "noseRightCorner",
+  "noseLeftCorner",
+  "rightCheek",
+  "leftCheek",
+];
+
+const drawMask = (ctx, keypoints) => {
+  Object.keys(facePoints).forEach((key) => {
+    const points = facePoints[key];
+    ctx.beginPath();
+
+    ctx.moveTo(points[0][0], points[0][1]);
+    for (let i = 1; i < points.length; i++) {
+      ctx.lineTo(keypoints[points[i]][0], keypoints[points[i]][1]);
+    }
+    ctx.closePath();
+    ctx.stroke();
+  });
+};
+
+export const draw = (predictions, ctx, width, height) => {
   if (predictions.length > 0) {
     predictions.forEach((prediction) => {
       const keypoints = prediction.scaledMesh;
-      const boundingBox = prediction.boundingBox;
-      const bottomRight = boundingBox.bottomRight;
-      const topLeft = boundingBox.topLeft;
-      // make the drawing mask larger a bit
-      const distance =
-        Math.sqrt(
-          Math.pow(bottomRight[0] - topLeft[0], 2) +
-            Math.pow(topLeft[1] - topLeft[1], 2)
-        ) * 0.02;
+      const annotations = prediction.annotations;
+
       ctx.clearRect(0, 0, width, height);
       ctx.fillStyle = "black";
       ctx.save();
-      ctx.beginPath();
-      drawMask(ctx, keypoints, distance);
-      ctx.closePath();
-      ctx.fill();
+      drawMask(ctx, keypoints);
       ctx.restore();
     });
   }
