aa8354acba26701dd7eb0470b2ee5bbf9bcaceea
Maxwell Stone <38924020+maxhs2014@users.noreply.github.com>
2021-12-11 13:35:53 -0500

    auth working

--- src/App.js
+++ src/App.js
@@ -1,41 +1,26 @@
 import React from 'react';
-import './App.css';
+import '@aws-amplify/ui/dist/style.css';
 import logo from './logo.svg';
-import Amplify from 'aws-amplify';
+import Amplify, {Auth} from 'aws-amplify';
 import {
-	AmplifyAuthenticator,
-	AmplifyContainer,
-	AmplifySignOut,
+	withAuthenticator
 } from '@aws-amplify/ui-react';
-import { AuthState, onAuthUIStateChange } from '@aws-amplify/ui-components';
 import awsconfig from './aws-exports';
 
 Amplify.configure(awsconfig);
+Auth.configure(awsconfig);
 
-const App = () => {
-	const [authState, setAuthState] = React.useState();
-	const [user, setUser] = React.useState();
+const App = ({signOut, user}) => {
 
-	React.useEffect(() => {
-		return onAuthUIStateChange((nextAuthState, authData) => {
-			setAuthState(nextAuthState);
-			setUser(authData);
-		});
-	}, []);
-
-	return authState === AuthState.SignedIn && user ? (
+	return (
 		<div className="App">
 			<header className="App-header">
 				<div>Hello, {user.username}</div>
 				<img src={logo} className="App-logo" alt="logo" />
-				<AmplifySignOut />
+				<button onClick={signOut}>Sign out</button>
 			</header>
 		</div>
-	) : (
-		<AmplifyContainer>
-			<AmplifyAuthenticator />
-		</AmplifyContainer>
-	);
+	)
 };
 
-export default App;
+export default withAuthenticator(App);
